c5723703f723cafc9bd604d0d8d2b9a02af80849
jhordycamacas <jhordycamacas9@gmail.com>
2025-06-04 12:45:45 -0500

    Add files via upload

---
+++ Practica.java
@@ -0,0 +1,13 @@
+package com.mycompany.practica;
+
+public class Practica {
+
+    public static void main(String[] args) {
+        Perro p = new Perro("Mamifero", 5,"Labrador");
+        p.ladrar();
+        p.describir();
+        p.calcularEdad();
+        p.vacunas();
+
+    }
+}
